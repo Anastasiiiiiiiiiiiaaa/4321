1cbfd7f9116caa011fa06a76de0fbc1421a99ae3
Anastasiiiiiiiiiiiaaa <145755357+Anastasiiiiiiiiiiiaaa@users.noreply.github.com>
2024-06-28 16:20:42 +0300

    Add files via upload

---
+++ javascripts/java-script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  let about = document.querySelector('.about');
+  about.addEventListener('mouseenter', function(){
+    about.classList.add('none');
+    
+
+  })
+
+  
+  
+
+});
